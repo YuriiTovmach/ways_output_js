f63ec85569af3847d6f6c62ddbb7b32e631d6b52
Yurii Tovmach <portfolio_yurii_tovmach@ukr.net>
2019-06-13 17:19:31 +0300

    first commit ways_output_js

---
+++ 1.js
@@ -0,0 +1,14 @@
+var p;
+p = document.getElementById('out');
+
+function f1(){p.innerHTML = 'one';}
+function f2(){d && p.innerHTML += ' <b>two</b>';}
+function f3(){p.innerHTML = p.innerHTML + 'three';}
+function f4(){p.innerText += ' <b>four</b>';}
+function f5(){p.insertAdjacentHTML('beforeBegin','five');}
+function f6(){p.insertAdjacentHTML('afterBegin','six');}
+function f7(){p.insertAdjacentHTML('beforeEnd','seven');}
+function f8(){p.insertAdjacentHTML('beforeEnd','eight');}
+function f9(){p.insertAdjacentHTML('afterEnd','nine');}
+function f10(){p.outerHTML = '<div class="one">ten</div>';}
+
